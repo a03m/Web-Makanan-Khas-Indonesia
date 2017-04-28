04c6c24565ced6d492ab997efc7d697ec0c7bbcd
fadhalshulhan <fadhal.shulhan@gmail.com>
2017-04-28 21:18:44 +0700

    Web

---
+++ js/jav.js
@@ -0,0 +1,33 @@
+function tampilubah(){
+	var a = document.getElementById("tampilkan1");
+	if (a.options[a.selectedIndex].value == 1){
+		window.alert("Mie Aceh");
+	}
+	else if (a.options[a.selectedIndex].value == 2){
+		alert("Bika Ambon");
+	}
+	else if (a.options[a.selectedIndex].value == 3){
+		alert("Rendang");
+	}
+	else if (a.options[a.selectedIndex].value == 4){
+		alert("Otak-otak");
+	}
+	else if (a.options[a.selectedIndex].value == 5){
+		alert("Sate Bandeng");
+	}
+	else if (a.options[a.selectedIndex].value == 6){
+		alert("Kerak Telor");
+	}
+	else if (a.options[a.selectedIndex].value == 7){
+		alert("Ayam Bertutu");
+	}
+	else if (a.options[a.selectedIndex].value == 8){
+		alert("Bubur Pedas");
+	}
+	else if (a.options[a.selectedIndex].value == 9){
+		alert("Ayam Cincane");
+	}
+	else if (a.options[a.selectedIndex].value == 10){
+		alert("Ikan Asar");
+	}
+}
